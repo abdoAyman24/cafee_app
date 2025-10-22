41a9cbce66a549bf63d801f9356292fa43da15a4
abdoAyman24 <abdelftahayman77@gmail.com>
2025-10-22 19:50:30 +0300

    finish fireBase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:caffee/Core/helper/on_generate_route.dart';
 import 'package:caffee/Core/utils/AppTheme.dart';
 import 'package:caffee/Feature/Auth/Presentation/View/sign_in_view.dart';
+import 'package:caffee/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   const MyApp({super.key});
 
   // This widget is the root of your application.
